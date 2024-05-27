cd1a9e85b4731bf3e61de311fb0dd5ced65eaa72
rz <romazu@github.com>
2024-05-27 12:39:32 +0400

    Look at AccessControlledOffchainAggregator contract example

--- generated/src/main.rs
+++ generated/src/main.rs
@@ -8,6 +8,7 @@ mod generated_contract;
 fn main() {
     let contract = generated_contract::Contract::new_from_position(U256::zero(), 0);
 
+    // // Example contract
     // println!("{:?}", contract.myNestedMapping.get_item(0u64));
     // println!("{:?}", contract.myNestedMapping.get_item(0u64).get_item(1u64));
     // println!("{:?}", contract.myMapping2.get_item("0u64"));
@@ -27,6 +28,7 @@ fn main() {
     // println!("{:?}", contract.staticArrayLarge.capacity());
     // // println!("{:?}", contract.staticArrayLarge.get_item(2).slot()); // panic
 
+    // // Uniswap V3
     // println!("{:?}", contract.slot0);
     // println!("{:?}", contract.ticks);
     // println!("{:?}", contract.ticks.get_item(42u64).initialized.slot());
@@ -38,20 +40,28 @@ fn main() {
     // println!("{:?}", contract.ticks.get_item(887270).slot());
     // println!("{:?}", contract.ticks.get_item(-92110).slot());
 
-    println!("totalSupply {:?}", contract.totalSupply.position());
-    println!("balanceOf {:?}", contract.balanceOf.position());
-    println!("allowance {:?}", contract.allowance.position());
-    println!("DOMAIN_SEPARATOR {:?}", contract.DOMAIN_SEPARATOR.position());
-    println!("nonces {:?}", contract.nonces.position());
-    println!("factory {:?}", contract.factory.position());
-    println!("token0 {:?}", contract.token0.position());
-    println!("token1 {:?}", contract.token1.position());
-    println!("reserve0 {:?}", contract.reserve0.position());
-    println!("reserve1 {:?}", contract.reserve1.position());
-    println!("blockTimestampLast {:?}", contract.blockTimestampLast.position());
-    println!("price0CumulativeLast {:?}", contract.price0CumulativeLast.position());
-    println!("price1CumulativeLast {:?}", contract.price1CumulativeLast.position());
-    println!("kLast {:?}", contract.kLast.position());
-    println!("unlocked {:?}", contract.unlocked.position());
-    println!("unlocked {:?}", contract.unlocked.position());
+    // // Uniswap V2
+    // println!("totalSupply {:?}", contract.totalSupply.position());
+    // println!("balanceOf {:?}", contract.balanceOf.position());
+    // println!("allowance {:?}", contract.allowance.position());
+    // println!("DOMAIN_SEPARATOR {:?}", contract.DOMAIN_SEPARATOR.position());
+    // println!("nonces {:?}", contract.nonces.position());
+    // println!("factory {:?}", contract.factory.position());
+    // println!("token0 {:?}", contract.token0.position());
+    // println!("token1 {:?}", contract.token1.position());
+    // println!("reserve0 {:?}", contract.reserve0.position());
+    // println!("reserve1 {:?}", contract.reserve1.position());
+    // println!("blockTimestampLast {:?}", contract.blockTimestampLast.position());
+    // println!("price0CumulativeLast {:?}", contract.price0CumulativeLast.position());
+    // println!("price1CumulativeLast {:?}", contract.price1CumulativeLast.position());
+    // println!("kLast {:?}", contract.kLast.position());
+    // println!("unlocked {:?}", contract.unlocked.position());
+
+    // AccessControlledOffchainAggregator ETH / USD
+    println!("owner {:?}", contract.owner.position());
+    println!("s_description {:?}", contract.s_description.position());
+    println!("s_transmitters {:?}", contract.s_transmitters.position());
+    println!("s_transmitters[0] {:?}", contract.s_transmitters.get_item(0).position());
+    println!("s_transmitters[1] {:?}", contract.s_transmitters.get_item(1).position());
+    println!("s_signers[0] {:?}", contract.s_signers.get_item(0).position());
 }
