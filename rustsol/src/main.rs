a3c83a830115f3bb8b8652673e221e64a85cea6a
rz <romazu@github.com>
2024-05-30 03:40:53 +0400

    Add a mandatory 'command' CLI argument

--- rustsol/src/main.rs
+++ rustsol/src/main.rs
@@ -4,6 +4,9 @@ use clap::Parser;
 /// Command line arguments
 #[derive(Parser)]
 struct Args {
+    /// Rustsol command. Currently only generate_storage_bindings is supported.
+    command: String,
+
     /// Path to the storage layout JSON file
     storage_layout_path: String,
 
@@ -19,6 +22,11 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
+    if args.command != "generate_storage_bindings" {
+        eprintln!("Error: Unsupported command '{}'. Currently only 'generate_storage_bindings' is supported.", args.command);
+        std::process::exit(1);
+    }
 
     rustsol::generate_storage_bindings(
         args.storage_layout_path.into(),
