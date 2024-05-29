1ba1e848567d181fabc4c23c825485841c83c85d
rz <romazu@github.com>
2024-05-30 01:16:08 +0400

    Refactor main.rs

--- rustsol/src/main.rs
+++ rustsol/src/main.rs
@@ -1,12 +1,4 @@
-use std::fs::File;
-use std::io::{Read, Write};
-
-mod layout;
-
-mod utils;
-mod types;
-mod generate;
-
+use rustsol;
 
 fn main() {
     let storage_layout_path = "example/solc_output.json";
@@ -15,18 +7,10 @@ fn main() {
     // let (contract_path, contract_name) = ("contract.sol", "MyContract");
     let (contract_path, contract_name) = ("UniswapV3Pool.sol", "UniswapV3Pool");
 
-    let mut file = File::open(storage_layout_path).expect("Cannot open storage layout json file");
-    let mut storage_layout_json_string = String::new();
-    file.read_to_string(&mut storage_layout_json_string).expect("Cannot read storage layout json file");
-    let solc_output: layout::SolcOutput = serde_json::from_str(&storage_layout_json_string).expect("JSON was not well-formatted");
-    let storage_layout = &solc_output.contracts[contract_path][contract_name].storage_layout;
-
-    let nested_types = storage_layout.traverse(contract_name.into());
-    let generated_tokens = generate::generate_structs(nested_types);
-
-    let syntax_tree = syn::parse_file(&generated_tokens.to_string()).expect("Failed to parse TokenStream");
-    let pretty_formatted_code = prettyplease::unparse(&syntax_tree);
-
-    let mut file = File::create(generated_file_path).expect("Unable to create file");
-    file.write_all(pretty_formatted_code.as_bytes()).expect("Unable to write data");
+    rustsol::generate(
+        storage_layout_path.into(),
+        generated_file_path.into(),
+        contract_path.into(),
+        contract_name.into(),
+    );
 }
