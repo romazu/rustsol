b30d8d12961b88deb855f310ed5edec72097ad3e
rz <romazu@github.com>
2024-05-29 12:01:42 +0400

    Get contract sources from contracts dir

--- pytools/storage_layout.py
+++ pytools/storage_layout.py
@@ -23,19 +23,27 @@ def get_solc_input_json(contract_sources: Dict[str, str]):
     return res
 
 
-contracts_dir = '../example/contracts/simple'
-contract_relpath = 'contract.sol'
-output_path = '../example/solc_output.json'
-solc_version = "v0.8.26"
-with open(os.path.join(contracts_dir, contract_relpath)) as fp:
-    contract_source_content = fp.read()
-contract_sources = {
-    contract_relpath: contract_source_content
-}
-solcx.install_solc(version=solc_version, show_progress=True)
-results = solcx.compile_standard(
-    input_data=get_solc_input_json(contract_sources),
-    solc_version=solc_version,
-)
-with open(output_path, 'w') as fp:
-    json.dump(results, fp, indent=2)
+
+if __name__ == '__main__':
+    output_path = '../example/solc_output.json'
+
+    contracts_dir = '../example/contracts/simple'
+    solc_version = "v0.8.26"
+
+    contract_sources = {}
+    for root, _, files in os.walk(contracts_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path) as fp:
+                contract_source_content = fp.read()
+            contract_reldir = os.path.relpath(root, contracts_dir)
+            contract_path = os.path.normpath(os.path.join(contract_reldir, file))
+            contract_sources[contract_path] = contract_source_content
+
+    solcx.install_solc(version=solc_version, show_progress=True)
+    results = solcx.compile_standard(
+        input_data=get_solc_input_json(contract_sources),
+        solc_version=solc_version,
+    )
+    with open(output_path, 'w') as fp:
+        json.dump(results, fp, indent=2)
--- rustsol/src/main.rs
+++ rustsol/src/main.rs
@@ -12,36 +12,23 @@ mod generate;
 
 
 fn main() {
-    let mut file = File::open("example/solc_output.json").expect("Cannot open storage layout json file");
+    let storage_layout_path = "example/solc_output.json";
+    let generated_file_path = "example/src/generated_contract.rs";
+
+    let (contract_path, contract_name) = ("contract.sol", "MyContract");
+
+    let mut file = File::open(storage_layout_path).expect("Cannot open storage layout json file");
     let mut storage_layout_json_string = String::new();
     file.read_to_string(&mut storage_layout_json_string).expect("Cannot read storage layout json file");
     let solc_output: layout::SolcOutput = serde_json::from_str(&storage_layout_json_string).expect("JSON was not well-formatted");
-    let storage_layout = &solc_output.contracts["contract.sol"]["MyContract"].storage_layout;
-    // println!("{:#?}", storage_layout);
-    // println!("{:#?}", storage_layout.types["t_uint32"]);
-    // println!("{:#?}", storage_layout.types["t_array(t_uint112)10_storage"]);
-    // println!("{:#?}", storage_layout.members[0]);
-
-    // let member = &storage_layout.members[6];
-    // let ftype = &storage_layout.types[&member.type_name];
-
-    // // let ket_ftype_name = &storage_layout.types[]
-    // println!("{:#?}", member);
-    // println!("{:#?}", ftype);
+    let storage_layout = &solc_output.contracts[contract_path][contract_name].storage_layout;
 
     let nested_types = storage_layout.traverse();
-    // for nested_type in &nested_types {
-    //     println!("{:?}", nested_type);
-    // }
-
     let generated_tokens = generate::generate_structs(nested_types);
-    // println!("{}", generated_tokens);
 
-    // Convert TokenStream to a pretty-printed string
     let syntax_tree = syn::parse_file(&generated_tokens.to_string()).expect("Failed to parse TokenStream");
-    let formatted_code = prettyplease::unparse(&syntax_tree);
+    let pretty_formatted_code = prettyplease::unparse(&syntax_tree);
 
-    let file_path = "example/src/generated_contract.rs";
-    let mut file = File::create(file_path).expect("Unable to create file");
-    file.write_all(formatted_code.as_bytes()).expect("Unable to write data");
+    let mut file = File::create(generated_file_path).expect("Unable to create file");
+    file.write_all(pretty_formatted_code.as_bytes()).expect("Unable to write data");
 }
